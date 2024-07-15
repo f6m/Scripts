94ed9d853023157e7303789ac2324f579b9ad8d1
f6m <mgf6mail@gmail.com>
2024-07-15 00:20:15 -0600

    Create apart.py

---
+++ edo/laplace/apart.py
@@ -0,0 +1,8 @@
+from sympy import *
+t,s=symbols('t,s')
+f=(-3*s**2+10*s-4)/((s-1)*(s-2)**2)
+print(apart(f))
+def y(t): return 3*exp(t)-6*exp(2*t)+4*t*exp(2*t)
+print(simplify(diff(y(t),t,2)-3*diff(y(t),t,1)+2*y(t)))
+#3/(s - 1) - 6/(s - 2) + 4/(s - 2)**2
+#4*exp(2*t)
