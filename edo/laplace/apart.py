53cdc50b44f9098758b20af10af32933b3c8ba78
f6m <mgf6mail@gmail.com>
2024-12-10 00:05:21 -0600

    Update apart.py

--- edo/laplace/apart.py
+++ edo/laplace/apart.py
@@ -2,8 +2,10 @@ from sympy import *
 t,s=symbols('t,s')
 f=(-3*s**2+10*s-4)/((s-1)*(s-2)**2)
 g=1/((s**2)*(s+1)**2)
+h=1/((s+1)*(s+4))
 print(apart(f))
 print(apart(g))
+print(apart(h))
 def y(t): return 3*exp(t)-6*exp(2*t)+4*t*exp(2*t)
 print(simplify(diff(y(t),t,2)-3*diff(y(t),t,1)+2*y(t)))
 #3/(s - 1) - 6/(s - 2) + 4/(s - 2)**2
