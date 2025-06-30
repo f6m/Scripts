d2f47dfcd48b01a53aa0a55ad8f603097e820e3f
f6m <mgf6mail@gmail.com>
2025-06-29 22:47:20 -0600

    Update vp.py

--- edo/vp.py
+++ edo/vp.py
@@ -1,16 +1,29 @@
-#Variacion de parametros made easy
-#integrate puede no devolver solucion
 from sympy import * 
 x=Symbol('x')
-A=Matrix([[1,exp(-x)],[0,-exp(-x)]])
-def y(x): return exp(x)
+
+#Ecuacion diferencial: y^{(2)}+y^{(1)}=x+e^x
+#Funciones del conjunto fundamental de soluciones
+def y1(x): return 1
+def y2(x): return exp(-x)
+
+#Colocamos la matriz de Wronski y la funcion de entrada
+A=Matrix([[y1(x),y2(x)],[diff(y1(x),x),diff(y2(x),x)]])
 b=Matrix([0,exp(x)+x])
+
+#Calculamos la inversa de la matriz de Wronski para resolver 
+#el sistema de ecuaciones
 C=A.inv()*b
 print(simplify(C))
+
+#Integramos las soluciones del sistema de ecuaciones
 c1=integrate(C[0],x)
 c2=integrate(C[1],x)
 print(c1)
 print(c2)
+
+#Construimos la solucion particular a partir de las integrales
 yp=simplify(c1+c2*exp(-x))
 print(yp)
+
+#Comprobamos la solucion
 print(diff(yp,x,2)+diff(yp,x,1))
