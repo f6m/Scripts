88e72dbcef7489c2e882884685abc91dab2d3cdd
f6m <mgf6mail@gmail.com>
2026-03-04 18:37:00 -0600

    Update statistical calculations in mmm.py

--- ps/mmm.py
+++ ps/mmm.py
@@ -1,14 +1,22 @@
 from numpy import *
 from scipy import stats
-vel = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+def prV(s): print("\033[92m {}\033[00m".format(s))
+vel = [1,2,3,1,1,3,4,5,2,2,4,7,5,6]
 
 media = mean(vel)
 mediana = median(vel)
 moda = stats.mode(vel)
+mediarecortada = stats.trim_mean(vel,0.1)
 q = quantile(vel,[0,0.25,0.5,0.75,1])
 p = percentile(vel,.5)
+prV("Medidas de tendencia central (de la muestra)")
 print("media:",media)
 print("mediana:",mediana)
 print("moda:",moda[0])
+print("media recortada:",mediarecortada)
 print("cuartiles:",q)
 print("percentil 50:",p)
+prV("Medidas de variabilidad (de la muestra)")
+print("Rango:",max(vel)-min(vel))
+print("Varianza:",var(vel))
+print("Varianza:",std(vel))
