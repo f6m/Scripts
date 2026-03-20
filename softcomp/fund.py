d4e7a122e3aedbf803fc110d5e2ccf7ae3b802d0
f6m <mgf6mail@gmail.com>
2026-03-20 01:16:24 -0600

    Modify fund.py to analyze national vs foreign companies
    Updated the analysis to visualize the count of national and foreign companies. Added filtering for national companies and included a histogram for their density.

--- softcomp/fund.py
+++ softcomp/fund.py
@@ -5,11 +5,22 @@ import seaborn as sns
 
 #El archivo debe estar en el contenido del collab sample_data
 f = read_csv('/content/empresassoftwareACT1.csv',encoding='latin-1')
-anios=f['fundacion'] #solo la columna indicada
-# print(anios)
-df = DataFrame(anios, columns=["fundacion"])
-ax=sns.histplot(data=df,x="fundacion",stat="density",kde=True,cumulative=True,binwidth=3)
-# Colocamos las etiquetas
-ax.set_xlabel("Años", fontsize=12)
-ax.set_ylabel("Frecuencia relativa acumulada", fontsize=12)
+anios=f['Nacional'] #solo la columna indicada
+df = DataFrame(anios)
+numne = df["Nacional"].value_counts()
+yy=[numne[0],numne[1]]
+xx=['Nacionales','Extrangeras']
+ax=sns.barplot(y=yy,x=xx,palette=['skyblue','orange'])
+ax.bar_label(ax.containers[1])
+ax.set_ylabel("Cantidad", fontsize=12)
+ax.bar_label(ax.containers[0])
+
 show()
+
+filterdf = df[df["Nacional"] == 'N']
+print(filterdf)
+ax=sns.histplot(filterdf,stat="density",kde=True,cumulative=False,binwidth=3)
+# Colocamos las etiquetas
+#ax.set_xlabel("Nacional o Extrangera", fontsize=12)
+#ax.set_ylabel("Frecuencia relativa acumulada", fontsize=12)
+#show()
