905bafefcd07fe20cf64e4663a5a564437776736
f6m <mgf6mail@gmail.com>
2026-09-23 23:06:55 -0600

    Update statistics calculations and add histogram plot

--- ps/mmmcdpdesv.py
+++ ps/mmmcdpdesv.py
@@ -1,21 +1,29 @@
 from numpy import *
 from scipy import stats
+import matplotlib.pyplot as plt
 import random
 def prV(s): print("\033[92m {}\033[00m".format(s))
 
-r = random.sample(range(100),20)
+r = random.sample(range(100),70)
 print(r)
+histo, bins = histogram(r,bins=10)
+print(histo,bins)
+plt.hist(r,bins=10)
+plt.show()
+indexh = argmax(histo)
+print(indexh)
+moda = (bins[indexh]+bins[indexh+1])/2
 
 media = mean(r)
 mediana = median(r)
-moda = stats.mode(r)
+#moda = stats.mode(r)
 mediarecortada = stats.trim_mean(r,0.1)
 q = quantile(r,[0,0.25,0.5,0.75,1])
 p = percentile(r,.04)
 prV("Medidas de tendencia central (de la muestra)")
 print("media:",media)
 print("mediana:",mediana)
-print("moda:",moda[0])
+print("moda:",moda)
 print("media recortada:",mediarecortada)
 print("cuartiles:",q)
 print("percentil p4:",p)
